4d1386b5928202c820736020b03cf9a3d6e8b07b
Kouhei0425 <mk.ibra2.swd@ezweb.ne.jp>
2020-06-28 22:01:00 +0900

    fix1

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -58,5 +58,113 @@
                 </div>
             </div>
         </div>
+
+        <div id="map" style="height: 500px; width: 50%; margin: 2rem auto 0;"></div>
+        <button id="getcurrentlocation">現在地周辺のジムを探す</button>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAZBtXt-V93liPBKMv382fxIo2ptUtHOtw&libraries=places"
+        ></script>
+        <script type="text/javascript">
+            $(function(){
+
+                'use strict';
+                let map;
+                let service;
+                let infowindow;
+                let pyrmont = new google.maps.LatLng(35.690921,139.70025799999996);
+                createMap(pyrmont)
+
+                document.getElementById('getcurrentlocation').onclick = function() {
+                    geoLocationInit();
+                }
+
+                function geoLocationInit() {
+                    if (navigator.geolocation) {
+                        navigator.geolocation.getCurrentPosition(success, fail);
+
+                    } else {
+                        createMap(pyrmont);
+                    }
+                }
+
+                function success(position) {
+                    let currentLat = position.coords.latitude;
+                    let currentLng = position.coords.longitude;
+
+                    let pyrmont = new google.maps.LatLng(currentLat,currentLng);
+
+                    createMap(pyrmont)
+
+                    CurrentPositionMarker(pyrmont);
+                }
+
+                function fail(pyrmont) {
+                    createMap(pyrmont);
+                }
+
+                function createMap(pyrmont) {
+
+                    map = new google.maps.Map(document.getElementById('map'), {
+                        center: pyrmont,
+                        zoom: 15
+                    });
+                    nearbysearch(pyrmont)
+                }
+
+                function createMarker(latlng, icn, place)
+                {
+                    let marker = new google.maps.Marker({
+                        position: latlng,
+                        map: map
+                    });
+
+                    let placename = place.name;
+                    let contentString = `<div class="sample"><p id="place_name">${placename}</p></div>`;
+                    let infoWindow = new google.maps.InfoWindow({
+                        content:  contentString
+                    });
+
+
+                    marker.addListener('click', function() {
+                        infoWindow.open(map, marker);
+                    });
+
+                }
+
+                function CurrentPositionMarker(pyrmont) {
+                    let image = 'http://i.stack.imgur.com/orZ4x.png';
+                    let marker = new google.maps.Marker({
+                        position: pyrmont,
+                        map: map,
+                        icon: image
+                    });
+                    marker.setMap(map);
+                }
+
+                function nearbysearch(pyrmont) {
+                    let request = {
+                        location: pyrmont,
+                        radius: '1500',
+                        type: ['gym']
+                    };
+
+                    service = new google.maps.places.PlacesService(map);
+                    service.nearbySearch(request, callback);
+
+                    function callback(results, status) {
+                        if (status == google.maps.places.PlacesServiceStatus.OK) {
+                            for (var i = 0; i < results.length; i++) {
+                                var place = results[i];
+                                var latlng = place.geometry.location;
+                                var icn = place.icon;
+
+                                createMarker(latlng, icn, place);
+                            }
+                        }
+                    }
+                }
+            });
+        </script>
+
     </div>
 @endsection
